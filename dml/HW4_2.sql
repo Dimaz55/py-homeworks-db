d72a1ff0f3905ddd1c1dac731b32abc2664fd089
Dimaz55 <92859457+Dimaz55@users.noreply.github.com>
2022-01-23 13:54:41 +0600

    Add files via upload

---
+++ dml/HW4_2.sql
@@ -0,0 +1,19 @@
+SELECT name, release_year FROM albums
+	WHERE release_year=2018;
+
+SELECT name, duration FROM tracks
+	ORDER BY duration DESC
+	LIMIT 1;
+	
+SELECT name FROM tracks
+	WHERE duration >= '0:3:15';
+
+SELECT name FROM collections
+	WHERE release_year BETWEEN 2018 AND 2020;
+
+SELECT * FROM artists
+	WHERE name NOT LIKE '% %';
+
+SELECT name FROM tracks
+	WHERE name ILIKE '%my%';
+
